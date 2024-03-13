bdded1409df6f30623878038b04a3fdfb3dc6a44
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-03-13 12:12:37 +0700

    havent done

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,22 @@
 
    You can use pytest or unittest.
 """
+import unittest
+from counter import Counter
+
+
+class TestCounter(unittest.TestCase):
+    def test_singleton(self):
+        counter = Counter()
+        counter1 = Counter()
+        for i in range(5):
+            counter.increment()
+        self.assertEqual(counter1, counter)
+
+    def test_instance_share_the_same_count(self):
+        counter1 = Counter()
+        counter1.increment()
+        counter1.increment()
+        counter2 = Counter()
+        self.assertEqual(2, counter2.count)
+
